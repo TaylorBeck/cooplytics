39e9984bbccf5a258220ffa5668d77f97971a989
Taylor Beck <beck.taylorg@gmail.com>
2024-08-30 12:56:13 -0700

    Add logo files

--- src/api/chickenApi.js
+++ src/api/chickenApi.js
@@ -2,7 +2,7 @@ import { ref, onValue } from 'firebase/database';
 import { db } from './firebaseConfig';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 /**
  * Subscribes to chicken data for a specific farm
