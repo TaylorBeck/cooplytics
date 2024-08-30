6951660013de0aec9856a099da49a13fadec1c8f
Taylor Beck <beck.taylorg@gmail.com>
2024-08-30 00:28:45 -0700

    Update configurations

---
+++ README.md
@@ -0,0 +1,8 @@
+# PoultryPro
+
+This project is a frontend client that allows users to manage poultry farms and their associated data. It provides features for user authentication, farm management, poultry management, and data analysis.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
--- src/services/config.js
+++ src/services/config.js
@@ -1,9 +1,17 @@
+/* eslint-disable no-undef */
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
 export const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'chicken-farm-c649e.firebaseapp.com',
-  databaseURL: 'https://chicken-farm-c649e-default-rtdb.firebaseio.com',
-  projectId: 'chicken-farm-c649e',
-  storageBucket: 'chicken-farm-c649e.appspot.com',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
