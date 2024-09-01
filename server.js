e85e41e09787d201056bf36af9385d452adcd409
Taylor Beck <beck.taylorg@gmail.com>
2024-09-01 05:11:13 -0700

    Update vite chunk config

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+const express = require('express');
+const path = require('path');
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// Get the current directory
+const __dirname = path.resolve();
 
 const app = express();
 const port = process.env.PORT || 8080;
