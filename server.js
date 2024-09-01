c202f36dff5fe8d76b1429d08b680740ff17e4ac
Taylor Beck <beck.taylorg@gmail.com>
2024-09-01 02:01:53 -0700

    Serve files from wwwroot

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = process.env.PORT || 8080;
 
-// Serve static files from dist/
-app.use(express.static(path.join(__dirname, 'dist')));
+// Serve static files from the current directory
+app.use(express.static(__dirname));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
